822396336dc3652c8eac356872484ce4343d6a36
david <david@makina>
2015-10-14 12:13:13 +0200

    ^^

---
+++ src/java/hotel/HotelWS.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author riogemm.panopio
+ */
+@WebService(serviceName = "HotelWS")
+public class HotelWS {
+
+    @WebMethod(operationName = "consulta_libres")
+    public int consulta_libres(@WebParam(name = "id_hotel") int id_hotel, @WebParam(name = "fecha") int fecha) throws ClassNotFoundException, SQLException {
+        //TODO write your implementation code here:
+        Class.forName("org.sqlite.JDBC");
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\david.molins.goma\\Downloads\\sqlite-jdbc-3.7.2");
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT num_hab_libres FROM hotel_fecha where id_hotel = \""+ id_hotel +"\" and fecha = "+ fecha );
+            return Integer.parseInt(rs.getString("num_hab_libres"));
+        } catch(SQLException e) {
+                System.err.println(e.getMessage());
+        }
+        return 0;
+    }
+
+    /**
+     * Dados un identificador de hotel y una fecha, reserva una habitación si quedan habitaciones libres (incrementa el número de habitaciones ocupadas en esa fecha en el hotel).
+     * Si es posible realizar la reserva, esta operación retorna el número de habitaciones ocupadas que hay en el hotel.
+     * Si no es posible realizar la reserva, esta operación retorna -1.
+     */
+    
+    @WebMethod(operationName = "reserva_habitacion")
+    public int reserva_habitacion(@WebParam(name = "id_hotel") int id_hotel, @WebParam(name = "fecha") int fecha) throws ClassNotFoundException {
+        //TODO write your implementation code here:
+    Class.forName("org.sqlite.JDBC");
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\david.molins.goma\\Downloads\\sqlite-jdbc-3.7.2");
+            Statement statement = connection.createStatement();
+            ResultSet res = statement.executeQuery("SELECT num_hab_libres FROM hotel_fecha where id_hotel = \""+ id_hotel +"\" and fecha = "+ fecha);
+            if(0 < Integer.parseInt(res.getString("num_hab_libres"))){
+                Integer rs = statement.executeUpdate("UPDATE hotel_fecha SET num_hab_libres = num_hab_libres-1, num_hab_ocupadas = num_hab_ocupadas + 1 WHERE id_hotel = \""+ id_hotel +"\" and fecha = "+ fecha);
+                //return Integer.parseInt(rs.getString("num_hab_libres"));
+                if(rs>0) return rs;
+            }
+            return -1;
+        } catch(SQLException e) {
+                System.err.println(e.getMessage());
+        }
+        return 0;
+    }
+}
---
+++ src/java/vuelo/VueloWS.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vuelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author riogemm.panopio
+ */
+@WebService(serviceName = "VueloWS")
+public class VueloWS {
+
+    /**
+     * Dados un identificador de vuelo y una fecha, retorna el número de plazas que están libres
+     */
+    
+    @WebMethod(operationName = "consulta_libres")
+    public int consulta_libres(@WebParam(name = "id_vuelo") int id_vuelo, @WebParam(name = "fecha") int fecha) throws ClassNotFoundException {
+        //TODO write your implementation code here:
+        Class.forName("org.sqlite.JDBC");
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\david.molins.goma\\Downloads\\sqlite-jdbc-3.7.2");
+            Statement statement = connection.createStatement();
+            String id = Integer.toString(id_vuelo);
+            String date = Integer.toString(fecha);
+            ResultSet rs = statement.executeQuery("select num_plazas_max, num_plazas_ocupadas from vuelo_fecha where id_vuelo = \"" + id + "\" and fecha = " + date);
+            int max = rs.getInt("num_plazas_max");
+            int ocupadas = rs.getInt("num_plazas_ocupadas");
+            int libres = max - ocupadas;
+            System.out.println(libres);
+            return libres;
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return 0;
+    }
+
+    /**
+     * Dados un identificador de vuelo y una fecha, reserva una plaza si quedan plazas libres (incrementa el número de plazas ocupadas en un vuelo en una fecha.
+     * Si es posible realizar la reserva, esta operación retorna el número de plazas ocupadas que hay en el vuelo.
+     * Si no es posible realizar la reserva, esta operación retorna -1.
+     */
+    
+    @WebMethod(operationName = "reserva_plaza")
+    public int reserva_plaza(@WebParam(name = "id_vuelo") int id_vuelo, @WebParam(name = "fecha") int fecha) throws ClassNotFoundException {
+        //TODO write your implementation code here:
+        Class.forName("org.sqlite.JDBC");
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Rio\\Dropbox\\UPC\\AD\\P3\\practica3.sqlite");
+            Statement statement = connection.createStatement();
+            String id = Integer.toString(id_vuelo);
+            String date = Integer.toString(fecha);
+            ResultSet rs = statement.executeQuery("select num_plazas_max, num_plazas_ocupadas from vuelo_fecha where id_vuelo = \"" + id + "\" and fecha = " + date);
+            int max = rs.getInt("num_plazas_max");
+            int ocupadas = rs.getInt("num_plazas_ocupadas");
+            int libres = max - ocupadas;
+            System.out.println(libres);
+            if (libres > 0) {
+                rs = statement.executeQuery("update vuelo_fecha set num_plazas_ocupadas = num_plazas_ocupadas + 1 where id_vuelo = \"" + id + "\" and fecha = " + date);
+                System.out.println("S'ha reservat correctament. Ocupades: " + libres+1);
+                return libres;
+            }
+            else {
+                System.out.println("No hi ha places lliures");
+                return -1;
+            }
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return -1;
+    }
+}
