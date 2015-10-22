24246555f857e165feb6e6d9524db51ce4a811c9
Rio <Rio@Rio-PC>
2015-10-22 17:48:26 +0200

    AgenciaWS

--- src/java/hotel/HotelWS.java
+++ src/java/hotel/HotelWS.java
@@ -88,7 +88,7 @@ public class HotelWS {
                 Integer rs = statement.executeUpdate("UPDATE hotel_fecha SET num_hab_libres = num_hab_libres-1, num_hab_ocupadas = num_hab_ocupadas + 1 WHERE id_hotel = "+ id_hotel +" and fecha = "+ fecha);
                 //return Integer.parseInt(rs.getString("num_hab_libres"));
                 //if(rs > 0) return rs;
-                if(rs > 0) return res.getInt("num_hab_libres");
+                if(rs > 0) return (res.getInt("num_hab_libres") + 1);
             }
             return -1;
         } catch(SQLException e) {
